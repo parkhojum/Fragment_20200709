3c1d8f6ef33be52c624666a32ab5e3428f649912
박호준 <hankil417@naver.com>
2020-07-09 20:12:19 +0900

    프래그먼트 xml / 코틀린 클래스 연결

--- app/src/main/java/com/example/fragment_20200709/fragment/FirstFragment.kt
+++ app/src/main/java/com/example/fragment_20200709/fragment/FirstFragment.kt
@@ -1,9 +1,22 @@
 package com.example.fragment_20200709.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.fragment_20200709.R
 
 class FirstFragment : Fragment() {
 
-    
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.activity_fragmentfirst,container,false)
+
+    }
 
 }
